de0c504bdd41ff9a520a6289554e005e8add536b
AdamSkuse <a.t.skuse@gmail.com>
2017-12-07 19:23:55 -0500

    Completes lecture 39. Adds relationship between professors and courses

--- single-program.php
+++ single-program.php
@@ -20,6 +20,31 @@
       <?php the_content() ?>
     </div>
         <?php
+        $relatedProfessors = new WP_Query(array(
+          'posts_per_page' => -1,
+          'post_type' => 'professor',
+          'orderby' => 'title',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'related_programs',
+              'compare' => 'LIKE',
+              'value' => '"' . get_the_ID() . '"'
+            )
+          )
+        ));
+
+        if ($relatedProfessors->have_posts()) {
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+        while ($relatedProfessors->have_posts()) {
+          $relatedProfessors->the_post(); ?>
+          <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php }
+        }
+
+        wp_reset_postdata();
+
         $today = date('Ymd');
         $homepageEvents = new WP_Query(array(
           'posts_per_page' => 2,
@@ -37,12 +62,14 @@
             array(
               'key' => 'related_programs',
               'compare' => 'LIKE',
-              'value' => '"' . get_the_ID('') . '"'
+              'value' => '"' . get_the_ID() . '"'
             )
           )
-
         ));
 
+        if ($homepageEvents->have_posts()) {
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
         while ($homepageEvents->have_posts()) {
           $homepageEvents->the_post(); ?>
           <div class="event-summary">
@@ -70,6 +97,7 @@
             </div>
           </div>
         <?php }
+        }
         ?>
   </div>
 

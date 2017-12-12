7c96ff30ad0d1fcbf864fd873a526936d1fec482
AdamSkuse <a.t.skuse@gmail.com>
2017-12-12 14:01:00 -0500

    Completes lecture 43. Adds custom banner to professor pages.

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@ function university_features() {
   add_theme_support('post-thumbnails');
   add_image_size('professorLandscape', 400, 260, true);
   add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 650, true);
 }
 
 function university_adjust_queries($query) {

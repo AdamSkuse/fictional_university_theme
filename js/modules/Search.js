c3907a1e32df243ffede2e934a94ef06f1de568f
AdamSkuse <a.t.skuse@gmail.com>
2017-12-13 13:31:37 -0500

    Adds keyboard shortcuts for search overlay

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -7,21 +7,37 @@ class Search {
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
     this.events();
+    this.isOverlayOpen = false;
   }
 
   // 2. Events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
+    $(document).on("keyup", this.keyPressDispatcher.bind(this));
   }
 
   // 3. Methods
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active"); 
+    $("body").addClass("body-no-scroll");
+    this.isOverlayOpen = true;
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active"); 
+    $("body").removeClass("body-no-scroll");
+    this.isOverlayOpen = false;
+  }
+
+  keyPressDispatcher(e) {
+
+    if (e.keyCode == 83 && !this.isOverlayOpen) {
+      this.openOverlay();
+    }
+    if (e.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
   }
 }
 
--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -10564,6 +10564,7 @@ var Search = function () {
     this.closeButton = (0, _jquery2.default)(".search-overlay__close");
     this.searchOverlay = (0, _jquery2.default)(".search-overlay");
     this.events();
+    this.isOverlayOpen = false;
   }
 
   // 2. Events
@@ -10574,6 +10575,7 @@ var Search = function () {
     value: function events() {
       this.openButton.on("click", this.openOverlay.bind(this));
       this.closeButton.on("click", this.closeOverlay.bind(this));
+      (0, _jquery2.default)(document).on("keyup", this.keyPressDispatcher.bind(this));
     }
 
     // 3. Methods
@@ -10582,11 +10584,26 @@ var Search = function () {
     key: "openOverlay",
     value: function openOverlay() {
       this.searchOverlay.addClass("search-overlay--active");
+      (0, _jquery2.default)("body").addClass("body-no-scroll");
+      this.isOverlayOpen = true;
     }
   }, {
     key: "closeOverlay",
     value: function closeOverlay() {
       this.searchOverlay.removeClass("search-overlay--active");
+      (0, _jquery2.default)("body").removeClass("body-no-scroll");
+      this.isOverlayOpen = false;
+    }
+  }, {
+    key: "keyPressDispatcher",
+    value: function keyPressDispatcher(e) {
+
+      if (e.keyCode == 83 && !this.isOverlayOpen) {
+        this.openOverlay();
+      }
+      if (e.keyCode == 27 && this.isOverlayOpen) {
+        this.closeOverlay();
+      }
     }
   }]);
 

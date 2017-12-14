006a63da258652dd5b95ffe9b287d2578257be8a
AdamSkuse <a.t.skuse@gmail.com>
2017-12-13 23:33:44 -0500

    Completes lecture 56. Adds wp-api access to search

--- js/scripts-bundled.js
+++ js/scripts-bundled.js
@@ -10560,12 +10560,15 @@ var Search = function () {
   function Search() {
     _classCallCheck(this, Search);
 
+    this.resultsDiv = (0, _jquery2.default)("#search-overlay__results");
     this.openButton = (0, _jquery2.default)(".js-search-trigger");
     this.closeButton = (0, _jquery2.default)(".search-overlay__close");
     this.searchOverlay = (0, _jquery2.default)(".search-overlay");
     this.searchField = (0, _jquery2.default)("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.isSpinnerVisible = false;
+    this.previousValue;
     this.typingTimer;
   }
 
@@ -10578,7 +10581,7 @@ var Search = function () {
       this.openButton.on("click", this.openOverlay.bind(this));
       this.closeButton.on("click", this.closeOverlay.bind(this));
       (0, _jquery2.default)(document).on("keydown", this.keyPressDispatcher.bind(this));
-      this.searchField.on("keydown", this.typingLogic.bind(this));
+      this.searchField.on("keyup", this.typingLogic.bind(this));
     }
 
     // 3. Methods
@@ -10586,10 +10589,29 @@ var Search = function () {
   }, {
     key: "typingLogic",
     value: function typingLogic() {
-      clearTimeout(this.typingTimer);
-      this.typingTimer = setTimeout(function () {
-        console.log("timeout test");
-      }, 2000);
+      if (this.searchField.val() != this.previousValue) {
+        clearTimeout(this.typingTimer);
+
+        if (this.searchField.val()) {
+          if (!this.isSpinnerVisible) {
+            this.resultsDiv.html('<div class="spinner-loader"></div>');
+            this.isSpinnerVisible = true;
+          }
+          this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+        } else {
+          this.resultsDiv.html('');
+          this.isSpinnerVisible = false;
+        }
+      }
+
+      this.previousValue = this.searchField.val();
+    }
+  }, {
+    key: "getResults",
+    value: function getResults() {
+      _jquery2.default.getJSON('http://fictional-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
+        alert(posts[0].title.rendered);
+      });
     }
   }, {
     key: "openOverlay",
@@ -10608,7 +10630,7 @@ var Search = function () {
   }, {
     key: "keyPressDispatcher",
     value: function keyPressDispatcher(e) {
-      if (e.keyCode == 83 && !this.isOverlayOpen) {
+      if (e.keyCode == 83 && !this.isOverlayOpen && !(0, _jquery2.default)("input, textarea").is(':focus')) {
         this.openOverlay();
       }
       if (e.keyCode == 27 && this.isOverlayOpen) {

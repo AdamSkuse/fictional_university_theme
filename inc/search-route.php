2060a41fc2a1b02b8b5196b739d176ab1b3c0e20
AdamSkuse <a.t.skuse@gmail.com>
2017-12-14 16:54:48 -0500

    Completes lecture 63. Registers search route.

---
+++ inc/search-route.php
@@ -0,0 +1,15 @@
+<?php
+
+add_action('rest_api_init','universityRegisterSearch');
+
+function universityRegisterSearch() {
+  register_rest_route('university/v1', 'search', array(
+    'methods' => WP_REST_SERVER::READABLE,
+    'callback' => 'universitySearchResults'
+  ));
+}
+
+function universitySearchResults() {
+  return 'congratulations this is a route!';
+}
+

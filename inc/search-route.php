46a7c4ac14e21a11fe6d2d2d92b981e7302c19ab
AdamSkuse <a.t.skuse@gmail.com>
2017-12-14 17:44:48 -0500

    Completes lecture 66. Adds custom search route

--- inc/search-route.php
+++ inc/search-route.php
@@ -9,7 +9,58 @@ function universityRegisterSearch() {
   ));
 }
 
-function universitySearchResults() {
-  return 'congratulations this is a route!';
+function universitySearchResults($data) {
+  $mainQuery = new WP_Query(array(
+    'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
+    's' => sanitize_text_field($data['term'])
+  ));
+
+  $results = array(
+    'generalInfo' => array(),
+    'professors' => array(),
+    'programs' => array(),
+    'events' => array(),
+    'campuses' => array()
+  );
+
+  while ($mainQuery->have_posts()) {
+    $mainQuery->the_post();
+
+    if (get_post_type() == 'post' OR get_post_type() == 'page') {
+      array_push($results['generalInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+    ));
+    }
+
+    if (get_post_type() == 'professor') {
+      array_push($results['professors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+    ));
+    }
+
+    if (get_post_type() == 'program') {
+      array_push($results['programs'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+    ));
+    }
+
+    if (get_post_type() == 'event') {
+      array_push($results['events'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+    ));
+    }
+
+    if (get_post_type() == 'campus') {
+      array_push($results['campuses'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+    ));
+    }
+  }
+  return $results;
 }
 

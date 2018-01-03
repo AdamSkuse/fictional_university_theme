942e85070491eee426256919e22554ee8a9e5ac4
AdamSkuse <a.t.skuse@gmail.com>
2018-01-03 14:00:42 -0500

    Completes search overlay. Adds related campus to search results.

--- inc/search-route.php
+++ inc/search-route.php
@@ -44,6 +44,18 @@ function universitySearchResults($data) {
     }
 
     if (get_post_type() == 'program') {
+      $relatedCampuses = get_field('related_campus');
+
+      if ($relatedCampuses ) {
+        foreach($relatedCampuses as $campus) {
+          array_push($results['campuses'], array(
+            'title' => get_the_title($campus),
+            'permalink' => get_the_permalink($campus)
+          ));
+        }
+      }
+
+
       array_push($results['programs'], array(
         'title' => get_the_title(),
         'permalink' => get_the_permalink(),
@@ -91,12 +103,31 @@ function universitySearchResults($data) {
     }
 
     $programRelationshipQuery = new WP_Query(array(
-      'post_type' => 'professor',
+      'post_type' => array('professor', 'event'),
       'meta_query' => $programsMetaQuery 
     ));
 
     while($programRelationshipQuery->have_posts()) {
       $programRelationshipQuery->the_post();
+
+      if (get_post_type() == 'event') {
+        $eventDate = new DateTime(get_field('event-date'));
+        $description = null;
+
+          if (has_excerpt()) {
+            $description = get_the_excerpt();
+            } else {
+            $description = wp_trim_words(get_the_content(), 18);
+          }
+
+        array_push($results['events'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'month' => $eventDate->format('M'),
+          'day' => $eventDate->format('d'),
+          'description' => $description
+      ));
+      }
 
       if (get_post_type() == 'professor') {
         array_push($results['professors'], array(
@@ -108,6 +139,7 @@ function universitySearchResults($data) {
     } 
     
     $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+    $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
   }
 
   return $results;

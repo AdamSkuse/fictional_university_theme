0c94eb0c81c338541456559d0dcedc9f5af7b180
AdamSkuse <a.t.skuse@gmail.com>
2018-01-16 08:57:07 -0500

    Completes lecture 74 - finishes styling user login page

--- functions.php
+++ functions.php
@@ -116,3 +116,24 @@ function noSubsAdminBar() {
     show_admin_bar(false);
   }
 }
+
+// Customise login screen
+//
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl() {
+  return esc_url(site_url('/'));
+}
+
+add_action ('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS() {
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle() {
+  return get_bloginfo('name');
+}

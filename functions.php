f5a22dece7308f40de7a85447f4939f7b8d05141
AdamSkuse <a.t.skuse@gmail.com>
2018-02-01 13:52:02 -0500

    Completes lecture 82. Adds server-side note privacy

--- functions.php
+++ functions.php
@@ -138,3 +138,13 @@ add_filter('login_headertitle', 'ourLoginTitle');
 function ourLoginTitle() {
   return get_bloginfo('name');
 }
+
+// Force notes to be private
+add_filter('wp_insert_post_data', 'makeNotePrivate');
+
+function makeNotePrivate($data) {
+  if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
+    $data['post_status'] = "private";
+  }
+  return $data;
+}
